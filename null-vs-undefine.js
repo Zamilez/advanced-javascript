d70146413882caed6f5be3b4f684987b61834faf
Zamilez <zamilhossain6293@gmail.com>
2023-08-29 21:59:24 +0600

    undefined

---
+++ null-vs-undefine.js
@@ -0,0 +1,21 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+
+const premik = {Name:"smart dude", phn:7654890};
+console.log(premik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
