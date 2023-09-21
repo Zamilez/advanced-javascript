04c105c7793c7ac8a01f5e3a21c273676c28c145
Zamilez <zamilhossain6293@gmail.com>
2023-09-21 22:28:49 +0600

    closer

---
+++ scope.js
@@ -0,0 +1,19 @@
+let  bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var  mood = "happy";
+        mood = 'One';
+        mood  = 'Two';
+        mood = 'Three';
+        // console.log(mood);
+    }
+    console.log(day);
+    var day = 'Friday'
+    return result;
+}
+const output = sum(3, 7);
+
+// console.log(bonus);
+// console.log(output);
